5a9d6f72524dda60119f7fdd82b0678e8ae80d71
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2024-01-10 11:35:31 +0100

    dasflkhlfal

--- src/main.rs
+++ src/main.rs
@@ -31,12 +31,14 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Poe Cipher App");
-            ui.text_edit_multiline(&mut self.input);
-            if ui.button("Translate").clicked() {
-                self.output = Poe::translate(&self.input)
-            }
-            ui.label(&self.output);
+            ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+                ui.heading("Poe Cipher App");
+                ui.text_edit_multiline(&mut self.input);
+                if ui.button("Translate").clicked() {
+                    self.output = Poe::translate(&self.input)
+                }
+                ui.label(&self.output);
+            });
         });
     }
 }
